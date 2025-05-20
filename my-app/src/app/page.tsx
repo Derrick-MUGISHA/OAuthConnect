6b1fa4cc112d89b0898dc50dbf27294fa50550bb
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2025-05-20 13:23:23 +0200

    styling the css

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import { SignInButton } from '../components/sign-in-button';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home-page'>
             <h1>Welcome to My App</h1>
             <p>This is the home page.</p>
             <div>
