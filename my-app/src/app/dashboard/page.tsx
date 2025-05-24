387bb305bb25cf9eca28a8215ca6acb4d5bb543f
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2025-05-24 19:57:55 +0200

    auth protection

--- my-app/src/app/dashboard/page.tsx
+++ my-app/src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import { redirect } from "next/navigation";
 import Dashboard from "./dashboard";
+import { checkIsAuthenticated } from "@/src/lib/auth/checkIsAuthenticated";
 
 const DashboardPage: React.FC = async() => {
-    // TODO: Add authentication logic here
-    const isAuthenticated = true; // Replace with actual authentication logic
-    // const isAuthenticated = await checkIsAuthenticated(); // Replace with actual authentication logic
+    // Check if the user is authenticated
+    const isAuthenticated = await checkIsAuthenticated(); // Replace with actual authentication logic
     if (!isAuthenticated) {
         redirect('/auth/sign-in');
     } else {
